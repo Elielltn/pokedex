fea55af6c8db2b7d7a9d8e89c3998afb3f9420a1
Elielltn <126324244+Elielltn@users.noreply.github.com>
2024-04-11 19:12:32 -0300

    2.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var myRequest = "https://pokeapi.co/api/v2/pokemon/";
 let pokemons = [];
-
+let urlTodos = [];
 const app = {
   buscarTodos: function (request) {
     fetch(request)
@@ -8,11 +8,16 @@ const app = {
         return response.json();
       })
       .then(function (data) {
-        for (let i = 0; i < data.results.length; i++) {
-          app.buscarApenasUm(data.results[i].url);
-        }
         if (data.next != null) {
           app.buscarTodos(data.next);
+          for (let i = 0; i < data.results.length; i++) {
+            urlTodos.push(data.results[i].url);
+            // app.buscarApenasUm(data.results[i].url);
+          }
+        } else {
+          urlTodos.forEach((element) => {
+            app.buscarApenasUm(element);
+          });
         }
       });
   },
@@ -36,25 +41,41 @@ const app = {
             pokeType2: data.types[1].type.name,
           });
         }
+      })
+      .then(function () {
+        app.atualizarPokemons(pokemons);
       });
   },
   criarNovoPokemon: function (pokemon) {
-    if (pokemon.hasOwnProperty("pokeType2")) {
+    if (pokemon.pokeId > 10000) {
+      return undefined;
+    } else if (pokemon.hasOwnProperty("pokeType2")) {
       return `
     <div class="grid-item">
             <div class="img-container">
               <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.pokeId}.png"
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                pokemon.pokeId
+              }.png"
               alt=""
               class="item-img"
             />
             </div>
             <div class="item-details">
               <p class="number">Nº${pokemon.pokeId}</p>
-              <p class="name">${pokemon.pokeName}</p>
+              <p class="name">${
+                pokemon.pokeName.charAt(0).toUpperCase() +
+                pokemon.pokeName.substring(1)
+              }</p>
               <div class="types">
-                <p class="type1 ${pokemon.pokeType1}-type">${pokemon.pokeType1}</p>
-                <p class="type2 ${pokemon.pokeType2}-type">${pokemon.pokeType2}</p>
+                <p class="type1 ${pokemon.pokeType1}-type">${
+        pokemon.pokeType1.charAt(0).toUpperCase() +
+        pokemon.pokeType1.substring(1)
+      }</p>
+                <p class="type2 ${pokemon.pokeType2}-type">${
+        pokemon.pokeType2.charAt(0).toUpperCase() +
+        pokemon.pokeType2.substring(1)
+      }</p>
               </div>
             </div>
           </div>
@@ -64,16 +85,24 @@ const app = {
     <div class="grid-item">
             <div class="img-container">
               <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.pokeId}.png"
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                pokemon.pokeId
+              }.png"
               alt=""
               class="item-img"
             />
             </div>
             <div class="item-details">
               <p class="number">Nº${pokemon.pokeId}</p>
-              <p class="name">${pokemon.pokeName}</p>
+              <p class="name">${
+                pokemon.pokeName.charAt(0).toUpperCase() +
+                pokemon.pokeName.substring(1)
+              }</p>
               <div class="types">
-                <p class="type1 ${pokemon.pokeType1}-type">${pokemon.pokeType1}</p>
+              <p class="type1 ${pokemon.pokeType1}-type">${
+        pokemon.pokeType1.charAt(0).toUpperCase() +
+        pokemon.pokeType1.substring(1)
+      }</p>
               </div>
             </div>
           </div>
@@ -82,11 +111,16 @@ const app = {
   },
   atualizarPokemons: function (arr) {
     let output = "";
-    for (pokemon of arr) output = output + app.criarNovoPokemon(pokemon);
+    for (pokemon of arr) {
+      if (app.criarNovoPokemon(pokemon) === undefined) {
+        output = output;
+        break;
+      }
+      output = output + app.criarNovoPokemon(pokemon);
+    }
 
     document.querySelector(".grid").innerHTML = output;
   },
 };
 
 app.buscarTodos(myRequest);
-app.atualizarPokemons(pokemons)
