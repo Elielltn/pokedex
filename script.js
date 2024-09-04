c0ef01354f16d251949b7ad1f1dc596d4e184aa0
Eliel Lucas <liel.lucastj@gmail.com>
2024-09-04 16:17:35 -0300

    Restart

--- script.js
+++ script.js
@@ -1,126 +1,85 @@
-var myRequest = "https://pokeapi.co/api/v2/pokemon/";
-let pokemons = [];
-let urlTodos = [];
-const app = {
-  buscarTodos: function (request) {
-    fetch(request)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        if (data.next != null) {
-          app.buscarTodos(data.next);
-          for (let i = 0; i < data.results.length; i++) {
-            urlTodos.push(data.results[i].url);
-            // app.buscarApenasUm(data.results[i].url);
-          }
-        } else {
-          urlTodos.forEach((element) => {
-            app.buscarApenasUm(element);
-          });
-        }
-      });
-  },
-  buscarApenasUm: function (request) {
-    fetch(request)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        if (data.types.length < 2) {
-          pokemons.push({
-            pokeId: data.id,
-            pokeName: data.name,
-            pokeType1: data.types[0].type.name,
-          });
-        } else {
-          pokemons.push({
-            pokeId: data.id,
-            pokeName: data.name,
-            pokeType1: data.types[0].type.name,
-            pokeType2: data.types[1].type.name,
-          });
-        }
-      })
-      .then(function () {
-        app.atualizarPokemons(pokemons);
-      });
-  },
-  criarNovoPokemon: function (pokemon) {
-    if (pokemon.pokeId > 10000) {
-      return undefined;
-    } else if (pokemon.hasOwnProperty("pokeType2")) {
-      return `
-    <div class="grid-item">
-            <div class="img-container">
-              <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                pokemon.pokeId
-              }.png"
-              alt=""
-              class="item-img"
-            />
-            </div>
-            <div class="item-details">
-              <p class="number">Nº${pokemon.pokeId}</p>
-              <p class="name">${
-                pokemon.pokeName.charAt(0).toUpperCase() +
-                pokemon.pokeName.substring(1)
-              }</p>
-              <div class="types">
-                <p class="type1 ${pokemon.pokeType1}-type">${
-        pokemon.pokeType1.charAt(0).toUpperCase() +
-        pokemon.pokeType1.substring(1)
-      }</p>
-                <p class="type2 ${pokemon.pokeType2}-type">${
-        pokemon.pokeType2.charAt(0).toUpperCase() +
-        pokemon.pokeType2.substring(1)
-      }</p>
-              </div>
-            </div>
-          </div>
-    `;
-    } else {
-      return `
-    <div class="grid-item">
-            <div class="img-container">
-              <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                pokemon.pokeId
-              }.png"
-              alt=""
-              class="item-img"
-            />
-            </div>
-            <div class="item-details">
-              <p class="number">Nº${pokemon.pokeId}</p>
-              <p class="name">${
-                pokemon.pokeName.charAt(0).toUpperCase() +
-                pokemon.pokeName.substring(1)
-              }</p>
-              <div class="types">
-              <p class="type1 ${pokemon.pokeType1}-type">${
-        pokemon.pokeType1.charAt(0).toUpperCase() +
-        pokemon.pokeType1.substring(1)
-      }</p>
-              </div>
-            </div>
-          </div>
-    `;
-    }
-  },
-  atualizarPokemons: function (arr) {
-    let output = "";
-    for (pokemon of arr) {
-      if (app.criarNovoPokemon(pokemon) === undefined) {
-        output = output;
-        break;
-      }
-      output = output + app.criarNovoPokemon(pokemon);
-    }
+const grid = document.getElementById("grid");
+const pokemonCount = 20;
 
-    document.querySelector(".grid").innerHTML = output;
-  },
+const pokemons = [];
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemons(i);
+  }
 };
 
-app.buscarTodos(myRequest);
+const getPokemons = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // Extracting relevant data
+  const pokeData = {
+    id: data.id,
+    pokeName: data.name,
+    type1: data.types[0].type.name,
+    type2: data.types[1]?.type.name || null, // Some Pokémon may not have a second type
+  };
+
+  // Add the data to the pokemons array
+  pokemons.push(pokeData);
+
+  // Update the grid with the fetched Pokémon's data
+  updateGrid(pokeData);
+};
+
+const updateGrid = (pokeData) => {
+  const pokemonDiv = document.createElement("div");
+  pokemonDiv.className = "grid-item";
+
+  if (pokeData.type2 === null) {
+    pokemonDiv.innerHTML = `
+    <div class="img-container" id="img-container">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeData.id}.png"
+              alt=""
+              class="item-img"
+              id="item-img"
+            />
+          </div>
+          <div class="item-details" id="item-details">
+            <p class="number" id="number">Nº${pokeData.id}</p>
+            <p class="name" id="name">${pokeData.pokeName}</p>
+            <div class="types" id="types">
+              <p class="type1 ${pokeData.type1}-type" id="type1">${pokeData.type1.charAt(0).toUpperCase() + pokeData.type1.substring(1)}</p>
+
+            </div>
+          </div>
+  `;
+  } else {
+    pokemonDiv.innerHTML = `
+          <div class="img-container" id="img-container">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeData.id}.png"
+              alt=""
+              class="item-img"
+              id="item-img"
+            />
+          </div>
+          <div class="item-details" id="item-details">
+            <p class="number" id="number">Nº${pokeData.id}</p>
+            <p class="name" id="name">${pokeData.pokeName}</p>
+            <div class="types" id="types">
+              <p class="type1 ${pokeData.type1}-type" id="type1">${pokeData.type1.charAt(0).toUpperCase() + pokeData.type1.substring(1)}</p>
+              <p class="type1 ${pokeData.type2}-type" id="type2">${pokeData.type2.charAt(0).toUpperCase() + pokeData.type2.substring(1)}</p>
+            </div>
+          </div>
+    `
+  ;
+  }
+
+  grid.appendChild(pokemonDiv);
+};
+
+fetchPokemons();
+
+//  <p>ID: ${pokeData.id}</p>
+//  <p>Name: ${pokeData.pokeName}</p>
+//  <p>Type 1: ${pokeData.type1}</p>
+//  <p>Type 2: ${pokeData.type2 || "None"}</p>
